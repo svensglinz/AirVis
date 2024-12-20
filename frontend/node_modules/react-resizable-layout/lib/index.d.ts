efcecf47ac0194e6c4ee670cf66e40df1096523d
sven <svenglinz@gmail.com>
2024-12-20 13:43:10 +0100

    update

--- frontend/node_modules/react-resizable-layout/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Resizable from './Resizable';
-import useResizable from './useResizable';
-export default Resizable;
-export { useResizable };
-export * from './types';
-//# sourceMappingURL=index.d.ts.map
--- frontend/node_modules/react-resizable-layout/lib/useResizable.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Resizable, UseResizableProps } from './types';
-declare const useResizable: ({ axis, disabled, initial, min, max, reverse, step, shiftStep, onResizeStart, onResizeEnd, containerRef, }: UseResizableProps) => Resizable;
-export default useResizable;
-//# sourceMappingURL=useResizable.d.ts.map
